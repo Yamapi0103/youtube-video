cee39c24ad5f80595010f3d0dfdc7f6e318b12dd
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-08-11 21:03:00 +0800

    fixed

--- src/store/actions.js
+++ src/store/actions.js
@@ -55,7 +55,7 @@ export const actions = {
       getVideos({ id, ...param })
         .then((res) => {
           commit('setVideos', res.data.items)
-          commit('setPageInfo', res.data.pageInfo)
+          // commit('setPageInfo', res.data.pageInfo)
           resolve(res)
         })
         .catch((err) => {
