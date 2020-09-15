95b9bb5a0a75ecd44736884be447ba10e34f8159
yamapi0103 <yamapi0103@gmail.com>
2020-09-15 15:39:54 +0800

    降版axios至18. 可共用params在axios.create裡

--- src/api.js
+++ src/api.js
@@ -3,33 +3,36 @@ import axios from 'axios'
 const service = axios.create({
   // baseURL: process.env.VUE_APP_API_URL || '/web/api'
   baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    key: 'AIzaSyAV_riwJ0Ow9XM9CaO3w2_2BDrxkU9rTEU',
+  }
 })
 
-export function getVideos( param ) {
+export function getVideos(param) {
   return service.get('videos', {
     params: {
       ...param,
-      key: 'AIzaSyAV_riwJ0Ow9XM9CaO3w2_2BDrxkU9rTEU',
+      // key: 'AIzaSyAV_riwJ0Ow9XM9CaO3w2_2BDrxkU9rTEU',
     },
   })
 }
-export function getRegions(){
-  return service.get('i18nRegions',{
+export function getRegions() {
+  return service.get('i18nRegions', {
     params: {
       part: "snippet",
       hl: "zh_TW", // 以中文回傳
-      key: "AIzaSyAV_riwJ0Ow9XM9CaO3w2_2BDrxkU9rTEU"
+      // key: "AIzaSyAV_riwJ0Ow9XM9CaO3w2_2BDrxkU9rTEU"
     }
   })
 }
-export function searchVideos(searchText,param){
+export function searchVideos(searchText, param) {
   return service.get("search", {
     params: {
       type: "video",
       q: searchText,
       // maxResults: this.count,
       ...param,
-      key: "AIzaSyAV_riwJ0Ow9XM9CaO3w2_2BDrxkU9rTEU"
+      // key: "AIzaSyAV_riwJ0Ow9XM9CaO3w2_2BDrxkU9rTEU"
     }
   });
 }
